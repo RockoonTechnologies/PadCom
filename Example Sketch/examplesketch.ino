34e43bb3bcf88f845f2393949ef5ed45c3005c04
RockoonTechnologies <66290320+RockoonTechnologies@users.noreply.github.com>
2020-06-10 00:43:21 -0400

    added demo sketch

---
+++ Example Sketch/examplesketch.ino
@@ -0,0 +1,24 @@
+
+
+
+#include PadComLib.h
+
+void setup() {
+  Serial.begin(9600);  opens serial port, sets data rate to 9600 bps
+}
+
+void loop() {
+  padStatus(Abort);
+}
+
+
+
+#include PadComLib.h
+
+void setup() {
+  Serial.begin(9600);  opens serial port, sets data rate to 9600 bps
+}
+
+void loop() {
+  padStatus(Abort);
+}
